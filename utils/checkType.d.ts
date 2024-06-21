138e486fc32d8eed15bf11d01821d2958f105a27
tzuyi0817 <tzuyi0817@users.noreply.github.com>
2024-06-21 01:40:21 +0000

    deploy: a25a13c0662389fa30e82c1173205d87cf4fa306

--- utils/checkType.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const isArray: (arg: any) => arg is any[];
-export declare const isPlainObject: (value: unknown) => value is object;
-export declare const isObject: (value: unknown) => value is Record<any, any>;
--- utils/common.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isHaveValue(value: unknown): value is Array<unknown>;
