f4ccbc6f168d15a3908c8b77e08ef5a7d44b425f
tzuyi0817 <tzuyi0817@users.noreply.github.com>
2024-11-15 10:11:50 +0000

    deploy: 4b41fe91af33bf7d6c4b79190f96421f91de811f

---
+++ assets/index-BA2gIGD8.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./ComponentPdfCanvas-CiEHLrBs.js";import{u as p}from"./ComponentPdfCanvas-CiEHLrBs.js";import"./Demo-BGZ_HJUr.js";import"./index-BB_O2iuQ.js";export{o as default,p as useFabric};
