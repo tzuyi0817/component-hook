7b8e518957e8bab88949a3f11ee59c600fce57b0
tzuyi <a0988426059@gmail.com>
2025-01-28 13:41:44 +0800

    feat(picker): add `time-picker` demo

--- docs/src/components/component/ComponentTimePicker.vue
+++ docs/src/components/component/ComponentTimePicker.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Demo from '@/components/Demo.vue';
 import BasePicker from '@/examples/time-picker/Base.vue';
-
+import ColumnsTypePicker from '@/examples/time-picker/ColumnsType.vue';
+import CustomRangePicker from '@/examples/time-picker/CustomRange.vue';
+import LabelFormatterPicker from '@/examples/time-picker/LabelFormatter.vue';
 import installationMd from '@/markdowns/picker/installation.md?raw';
 import cssVariablesMd from '@/markdowns/picker/css-variables.md?raw';
 import { md } from '@/utils/highlight';
@@ -10,6 +12,12 @@ interface Props {
   frontEndFrame: string;
   baseSource: string;
   basePlayground: string;
+  columnsTypeSource: string;
+  columnsTypePlayground: string;
+  customRangeSource: string;
+  customRangePlayground: string;
+  labelFormatterSource: string;
+  labelFormatterPlayground: string;
   language?: string;
 }
 
@@ -38,6 +46,56 @@ defineProps<Props>();
         <p>When no time is specified, the current time is selected by default.</p>
       </template>
       <base-picker />
+    </demo>
+
+    <demo
+      title="Columns Type"
+      :source="columnsTypeSource"
+      :playground="columnsTypePlayground"
+      :language="language"
+    >
+      <template #description>
+        <p>
+          The type of options can be controlled through the <code>columns-type</code> attribute, which supports
+          permuting and combining hour, minute and second in any order.
+        </p>
+        <p>Three options of hour, minute and second are displayed by default.</p>
+        <ul class="mt-4 pl-8 list-disc">
+          <li><code>['hour']</code> represents the option to display only the hour.</li>
+          <li><code>['minute']</code> represents the option to display only the minute.</li>
+          <li><code>['hour', 'minute']</code> represents the option to display only the hour and minute.</li>
+          <li><code>['minute', 'second']</code> represents the option to display only the minute and second.</li>
+        </ul>
+      </template>
+      <columns-type-picker />
+    </demo>
+
+    <demo
+      title="Custom Time Range"
+      :source="customRangeSource"
+      :playground="customRangePlayground"
+      :language="language"
+    >
+      <template #description>
+        <p>Optional time ranges are set via the <code>min-time</code> and <code>max-time</code> properties, with the format <code>00:00:00</code>.</p>
+        <p>For example, in the following example, users can select any time between <code>12:20:30</code> and <code>16:30:48</code>.</p>
+      </template>
+      <custom-range-picker />
+    </demo>
+
+    <demo
+      title="Label Formatter"
+      :source="labelFormatterSource"
+      :playground="labelFormatterPlayground"
+      :language="language"
+    >
+      <template #description>
+        <p>
+          Display labels can be converted through the <code>format-hour-label</code>, <code>format-minute-label</code> and
+          <code>format-second-label</code> properties.
+        </p>
+      </template>
+      <label-formatter-picker />
     </demo>
 
     <h3>API</h3>
---
+++ docs/src/examples/time-picker/LabelFormatter.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { TimePicker, type PickerSelectedValues } from '@component-hook/picker/vue';
+
+const isShowPicker = ref(false);
+const currentTime = ref<PickerSelectedValues>([]);
+
+const HOURS_MAP: Record<string, string> = {
+  0: '12 AM',
+  1: '1 AM',
+  2: '2 AM',
+  3: '3 AM',
+  4: '4 AM',
+  5: '5 AM',
+  6: '6 AM',
+  7: '7 AM',
+  8: '8 AM',
+  9: '9 AM',
+  10: '10 AM',
+  11: '11 AM',
+  12: '12 PM',
+  13: '1 PM',
+  14: '2 PM',
+  15: '3 PM',
+  16: '4 PM',
+  17: '5 PM',
+  18: '6 PM',
+  19: '7 PM',
+  20: '8 PM',
+  21: '9 PM',
+  22: '10 PM',
+  23: '11 PM',
+}
+</script>
+
+<template>
+  <time-picker
+    v-model="currentTime"
+    v-model:show="isShowPicker"
+    title="Label Formatter Selector"
+    :format-hour-label="(hour: string) => HOURS_MAP[hour]"
+  />
+
+  <button @click="isShowPicker = true">toggle Label Formatter picker</button>
+
+  <p class="mt-6 text-sm font-mono">Selected time: {{ currentTime.join(':') || 'not selected yet' }}</p>
+</template>
