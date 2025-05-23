2fe2d0145891942080b94238aa800db1b922a3e3
tzuyi <a0988426059@gmail.com>
2025-05-23 16:43:38 +0800

    chore(create-app): remove unused files

--- packages/create-app/template-vue/src/__tests__/setup/unit-test.ts
+++
@@ -1,6 +0,0 @@
-import { mswServer } from '@/mocks/server';
-import '@testing-library/jest-dom';
-
-beforeAll(() => mswServer.listen());
-afterEach(() => mswServer.resetHandlers());
-afterAll(() => mswServer.close());
