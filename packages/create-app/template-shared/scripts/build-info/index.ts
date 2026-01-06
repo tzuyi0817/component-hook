5393a1e8cbf78046465092a70a3ec2fd71319b4b
tzuyi <a0988426059@gmail.com>
2026-01-06 16:22:48 +0800

    fix(create-app): errors that occurred when Git was not configured

--- packages/create-app/template-shared/scripts/build-info/index.ts
+++ packages/create-app/template-shared/scripts/build-info/index.ts
@@ -4,10 +4,16 @@ import packageJson from '../../package.json' with { type: 'json' };
 
 const platform = os.platform();
 const git = platform === 'win32' ? String.raw`C:\Program Files\Git\bin\git.exe` : '/usr/bin/git';
-const hash = execFileSync(git, ['rev-parse', 'HEAD']).toString().trim().slice(0, 7);
+let hash = 'unknown';
+
+try {
+  hash = execFileSync(git, ['rev-parse', 'HEAD']).toString().trim().slice(0, 7);
+} catch {
+  hash = 'no-git-detected';
+}
 
 process.env.VITE_APP_VERSION = packageJson.version;
-process.env.VITE_APP_LAST_COMMIT_HASH = `${hash}`;
+process.env.VITE_APP_LAST_COMMIT_HASH = hash;
 process.env.VITE_APP_BUILD_EPOCH = Date.now().toString();
 
 if (process.env.MOCK) {
